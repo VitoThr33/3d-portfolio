b489c5242c8482c4158f073ad39a68693b5bd376
Ryan McGowan <vitomaserati@gmail.com>
2023-04-11 16:42:48 -0700

    import and load assets getting error on room.js

---
+++ room-portfolio/Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name:"room",
+        type: "glbModel",
+        path: "/models/completedRoom.glb",
+    },
+    {
+        name:"screen",
+        type: "videoTexture",
+        path: "/textures/Nobi.mp4",  
+    }
+]
--- room-portfolio/Experience/World/World.js
+++ room-portfolio/Experience/World/World.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import Experience from "../Experience.js";
 
 import Room from "./Room.js";
+import Environment from "./Environment.js";
 
 export default class World {
     constructor (){
@@ -10,8 +11,14 @@ export default class World {
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
         this.camera = this.experience.camera;
+        this.resources = this.experience.resources;
+
+        this.resources.on("ready", ()=>{
+            this.room = new Room();
+        });
 
         this.room = new Room();
+        console.log("Room created")
     }
 
     resize(){
