5235f047f6dea907302ed14f2dba6f2a174adfd0
Ryan McGowan <vitomaserati@gmail.com>
2023-04-14 22:16:07 -0700

    fixed error

---
+++ room-portfolio/Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/fv16.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/javatricks.mp4"
+    },
+]
