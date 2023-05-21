5ed3fd558eaa644e4df488770a3baaafd5d2fca9
Ryan McGowan <vitomaserati@gmail.com>
2023-05-20 20:01:57 -0700

    styling done for test color 1

--- room-portfolio/Experience/World/Controls.js
+++ room-portfolio/Experience/World/Controls.js
@@ -6,7 +6,8 @@ import {ScrollTrigger} from "gsap/ScrollTrigger.js"
 export default class Controls {
     constructor (){
         this.experience = new Experience();
-        this.scene = this.experience.scene;
+        this.sizes = this.experience.scene;
+        this.sizes = this.experience.sizes;
         this.resources = this.experience.resources;
         this.time = this.experience.time;
         this.camera = this.experience.camera;
@@ -20,13 +21,16 @@ export default class Controls {
         console.log(this.room)
         this.timeline = new GSAP.timeline();
         this.timeline.to(this.room.position, {
-            x: 5,
+            x: ()=> {
+                return this.sizes.width*0.0012
+            },
             scrollTrigger:{
                 trigger:".first-move",
                 markers: true,
                 start: "top top",
                 end:"bottom bottom",
-                scrub: 0.8,
+                scrub: 0.5,
+                invalidateOnRefresh: true,
             }
         });
     }
--- room-portfolio/Experience/World/Environment.js
+++ room-portfolio/Experience/World/Environment.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import Experience from "../Experience.js";
+import GSAP from "gsap";
+import GUI from "lil-gui";
 
 export default class Environment {
     constructor (){
@@ -7,8 +9,26 @@ export default class Environment {
         this.scene = this.experience.scene;
         this.resources = this.experience.resources;
         
+      // this.gui = new GUI({container: document.querySelector(".hero-main")});
+        this.obj = {
+            colorObj: {r: 0, g: 0, b: 0},
+            intensity: 3,
+        };
         
         this.setSunlight();
+       // this.setGUI();
+    }
+
+    setGUI(){
+        this.gui.addColor(this.obj, "colorObj").onChange(()=>{
+            this.sunLight.color.copy(this.obj.colorObj);
+            this.ambientLight.color.copy(this.obj.colorObj);
+            console.log(this.obj.colorObj);
+        })
+        this.gui.add(this.obj, "intensity", 0, 10).onChange(()=>{
+            this.sunLight.intensity = this.obj.intensity;
+            this.sunLight.ambientLight = this.obj.intensity;
+        })
     }
 
     setSunlight(){
@@ -22,6 +42,44 @@ export default class Environment {
 
         this.ambientLight = new THREE.AmbientLight("#ffffff",1);
         this.scene.add(this.ambientLight);
+    }
+
+    switchTheme(theme){
+        if(theme === "dark"){
+            GSAP.to(this.sunLight.color,{
+                r: 0.011764705882352941,
+                g: 0.023529411764705882,
+                b: 0.16862745098039217,
+          });
+            GSAP.to(this.ambientLight.color,{
+                r: 0.011764705882352941,
+                g: 0.023529411764705882,
+                b: 0.16862745098039217,
+            });
+            GSAP.to(this.sunLight, {
+                intensity:2.78,
+            })
+            GSAP.to(this.ambientLight, {
+                intensity:2.78,
+            })
+           }else{
+            GSAP.to(this.sunLight.color,{
+                r:255 / 255,
+                g:255 / 255,
+                b:255 / 255,
+            });
+            GSAP.to(this.ambientLight.color,{
+                r:255 / 255,
+                g:255 / 255,
+                b:255 / 255,
+            });
+            GSAP.to(this.sunLight, {
+                intensity:3,
+            })
+            GSAP.to(this.ambientLight, {
+                intensity:3,
+            })
+           }
     }
 
     resize(){
